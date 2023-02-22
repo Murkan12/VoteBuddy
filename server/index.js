36f2412651ce0e02baf6862907c4ab418aa3bf3d
Murkan <loud3ds@gmail.com>
2023-02-22 20:31:37 +0100

    Add .env enviromental variable for database key
    Add database connection function and basic error handling
    Add server listener on 3000

--- server/index.js
+++ server/index.js
@@ -1,4 +1,26 @@
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
+
+const PORT = 3000;
+
+mongoose.set("strictQuery", false);
+
+const createRouter = require("./routes/Create");
+
+mongoose.connect(process.env.DATABASE_URL);
+
+const db = mongoose.connection;
+db.on("error", (error) => {
+  console.log(error);
+});
+db.once("open", () => console.log("Connected to database..."));
+
+app.use("/create", createRouter);
+
+app.listen(PORT, () => {
+  console.log("Connected to server...");
+});
