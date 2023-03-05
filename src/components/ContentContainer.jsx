f833872bfa88e383a14d4380f493beaf3fee30eb
Murkan12 <121810399+Murkan12@users.noreply.github.com>
2023-03-05 17:28:37 +0100

    Delete src directory

--- src/components/ContentContainer.jsx
+++
@@ -1,7 +0,0 @@
-export const ContentContainer = ({ children }) => {
-  return (
-    <div className="bg-slate-800 my-16 rounded-md w-96 drop-shadow-lg">
-      {children}
-    </div>
-  );
-};
--- src/components/TitleBox.jsx
+++
@@ -1,7 +0,0 @@
-export const TitleBox = ({ children }) => {
-  return (
-    <h1 className="text-5xl mt-14 font-bold drop-shadow-lg bg-orange-500 inline-block p-2 rounded-md">
-      <span className="drop-shadow-sm">{children}</span>
-    </h1>
-  );
-};
